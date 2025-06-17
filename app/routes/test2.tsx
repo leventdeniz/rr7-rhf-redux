e0f5c14a5c05928f8b4c1e4c53016835672c3420
levent.deniz <levent.deniz@check24.de>
2025-06-17 17:14:31 +0200

    use clientLoader

--- app/routes/test2.tsx
+++ app/routes/test2.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import PersonenFormular from "../components/persons-form/PersonenFormular";
-import { useAppSelector } from "~/store/hooks";
+import { loadFromStorage } from "~/store/middleware/persistanceMiddleware";
+import { useLoaderData } from "react-router";
+
+export const clientLoader = async () => {
+  const formData = loadFromStorage();
+  return { personsForm: formData?.personsForm };
+};
 
 export default function Test2() {
-  const formData = useAppSelector(state => state.forms.personsForm);
-  
+  const { personsForm: formDataFromLoader } = useLoaderData<typeof clientLoader>();
+
   return (
     <main className="flex items-center justify-center pt-8 pb-4">
       <div className="flex-1 flex flex-col items-center gap-8 min-h-0">
         <h1 className="text-2xl font-bold">Test-Seite 2</h1>
         <p>Dies ist eine weitere einfache Test-Seite.</p>
-        <PersonenFormular defaultValues={formData} />
+        <PersonenFormular defaultValues={formDataFromLoader} />
       </div>
     </main>
   );
